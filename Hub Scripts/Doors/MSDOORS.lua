0651203d66c0becb4484331d451ebc9d5621470d
Tom <162785970+graxe20@users.noreply.github.com>
2024-04-03 21:47:03 +0100

    V1 UPLOAD

---
+++ Hub Scripts/Doors/MSDOORS.lua
@@ -0,0 +1,5 @@
+--[[ THIS IS NOT MY SCRIPT - TOMHUB DOESN'T OWN THIS SCRIPT - WE PROVIDE SCRIPTS IN ONE PLACE, NOT MAKE.
+
+Original Pastebin Link: https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS.lua      ]]
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSHUB_Loader.lua"),true))()
